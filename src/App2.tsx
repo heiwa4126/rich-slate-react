acaab47c1f2daf34fb043f41816d553a32d4b2e5
heiwa4126 <heiwa4126@gmail.com>
2024-11-08 10:46:08 +0900

    [WIP] backup

---
+++ src/App2.tsx
@@ -0,0 +1,30 @@
+import { useMemo } from "react";
+import { type Descendant, createEditor } from "slate";
+import { withHistory } from "slate-history";
+import { Editable, Slate, withReact } from "slate-react";
+import { Aa, Template1 } from "./App";
+
+export function App() {
+	const editor = useMemo(() => withHistory(withReact(createEditor())), []);
+	return (
+		<Template1 title="2. すごくリッチ">
+			<div className="p-1 border-2">
+				<Slate editor={editor} initialValue={initialValue}>
+					<Editable placeholder="Enter some plain text..." />
+				</Slate>
+			</div>
+			<p className="mt-5">
+				参考: <Aa txt="richtext.tsx" />
+			</p>
+		</Template1>
+	);
+}
+
+const initialValue: Descendant[] = [
+	{
+		type: "paragraph",
+		children: [{ text: "This is editable plain text, just like a <textarea>!" }],
+	},
+];
+
+export default App;
